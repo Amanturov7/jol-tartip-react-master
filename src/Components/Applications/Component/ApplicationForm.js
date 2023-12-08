60ab27c0a6a7c0b42612686ed3d65d3d9bef3db8
Amanturov7 <edi.amanturov@gmail.com>
2023-12-08 17:17:40 +0600

    123

--- src/Components/Applications/Component/ApplicationForm.js
+++ src/Components/Applications/Component/ApplicationForm.js
@@ -74,12 +74,16 @@ const ApplicationForm = ({ onCancel }) => {
         
         <label>Description:</label>
         <textarea value={description} onChange={(e) => setDescription(e.target.value)} required />
-     
+    
       </div>
-
-      
-
-
+      <div className="form-group">
+        <label>Title:</label>
+        <input type="text" value={title} onChange={(e) => setPlace(e.target.value)} required />
+        
+        <label>Description:</label>
+        <textarea value={description} onChange={(e) => setDescription(e.target.value)} required />
+    
+      </div>
     </form>
   );
 };
