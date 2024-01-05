cefb61b718e34fa996f7e8731721436e72b8a8b2
Amanturov7 <edi.amanturov@gmail.com>
2024-01-05 16:43:46 +0600

    address map for application

--- src/Components/Applications/Component/ApplicationForm.js
+++ src/Components/Applications/Component/ApplicationForm.js
@@ -1,13 +1,11 @@
-// ApplicationForm.jsx
 import React, { useState, useEffect } from 'react';
 import Axios from 'axios';
+import MapComponent from './MapComponent';
 
 const ApplicationForm = ({ onCancel }) => {
   const [title, setTitle] = useState('');
   const [description, setDescription] = useState('');
   const [place, setPlace] = useState('');
-  const [lon, setLon] = useState('');
-  const [lat, setLat] = useState('');
   const [status, setStatus] = useState('');
   const [regionId, setRegionId] = useState('');
   const [file, setFile] = useState(null);
@@ -17,23 +15,22 @@ const ApplicationForm = ({ onCancel }) => {
   const [violationsList, setViolationsList] = useState([]);
   const [regions, setRegions] = useState([]);
   const [districts, setDistricts] = useState([]);
+  const [selectedCoordinate, setSelectedCoordinate] = useState({ lat: 0, lon: 0 });
+  const [isMapVisible, setIsMapVisible] = useState(false);
 
   useEffect(() => {
     const fetchRegionsAndDistricts = async () => {
       try {
         const regionsResponse = await Axios.get('http://localhost:8080/rest/common-reference/by-type/001');
         const districtsResponse = await Axios.get('http://localhost:8080/rest/common-reference/by-type/002');
-  
-        console.log('Regions response:', regionsResponse.data);
-        console.log('Districts response:', districtsResponse.data);
-  
+
         setRegions(regionsResponse.data);
         setDistricts(districtsResponse.data);
       } catch (error) {
         console.error('Error fetching regions and districts:', error.message);
       }
     };
-  
+
     fetchRegionsAndDistricts();
   }, []);
 
@@ -52,46 +49,53 @@ const ApplicationForm = ({ onCancel }) => {
 
   const handleFileChange = (e) => {
     const selectedFile = e.target.files[0];
-  
-    // Check if a file is selected
+
     if (selectedFile) {
-      // Check if the file type is supported (for example, allow only images)
-      const allowedFileTypes = ['image/jpeg', 'image/png']; // Add or modify allowed file types
-  
-      // Get the actual file type
+      const allowedFileTypes = ['image/jpeg', 'image/png'];
+
       const fileType = selectedFile.type;
-  
+
       if (allowedFileTypes.includes(fileType)) {
         setFile(selectedFile);
       } else {
-        alert('Файл не поддерживается. Выберите jpeg, jpg, png, mp4'); // You can replace this with a more user-friendly notification
-        e.target.value = null; // Clear the input field
+        alert('Файл не поддерживается. Выберите jpeg, jpg, png, mp4');
+        e.target.value = null;
         setFile(null);
       }
     }
   };
-  
-  
-  
+
+  const handleCoordinateSelect = ({ lat, lon }) => {
+    setSelectedCoordinate({ lat, lon });
+  };
+
+  const handleShowMap = () => {
+    setIsMapVisible(true);
+  };
+
+  const handleSaveCoordinates = () => {
+    setIsMapVisible(false);
+  };
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     if (!file) {
-      alert('Please select a file.'); 
+      alert('Please select a file.');
       return;
     }
     const newApplication = {
       title,
       description,
       place,
-      lon,
-      lat,
+      lat: selectedCoordinate.lat,
+      lon: selectedCoordinate.lon,
       status,
       regionId,
       districtId,
       typeViolationsId,
       userId,
     };
-  
+
     try {
       const applicationResponse = await Axios.post(
         'http://localhost:8080/rest/applications/create',
@@ -102,32 +106,31 @@ const ApplicationForm = ({ onCancel }) => {
           },
         }
       );
-  
-      console.log('Application created:', applicationResponse.data);
-  
+
       const formData = new FormData();
       formData.append('file', file);
-      formData.append('dto', JSON.stringify({
-        type: 'application',
-        originName: file ? file.name : '',
-        description: 'File description',  
-        userId: 1,  
-        applicationsId: applicationResponse.data.id,
-      }));
-  
+      formData.append(
+        'dto',
+        JSON.stringify({
+          type: 'application',
+          originName: file ? file.name : '',
+          description: 'File description',
+          userId: 1,
+          applicationsId: applicationResponse.data.id,
+        })
+      );
+
       await Axios.post('http://localhost:8080/rest/attachments/upload', formData, {
         headers: {
           'Content-Type': 'multipart/form-data',
         },
       });
-  
+
       onCancel();
-      // Reset form fields
       setTitle('');
       setDescription('');
       setPlace('');
-      setLon('');
-      setLat('');
+      setSelectedCoordinate({ lat: 0, lon: 0 });
       setStatus(0);
       setRegionId(0);
       setDistrictId(0);
@@ -138,16 +141,12 @@ const ApplicationForm = ({ onCancel }) => {
       console.error('Error creating application:', error.message);
     }
   };
-  
 
   return (
     <form onSubmit={handleSubmit} className="form-container">
       <div className="form-group">
         <label>Описание</label>
         <textarea value={description} onChange={(e) => setDescription(e.target.value)} required />
-
-        <label>Адрес</label>
-        <input type="text" value={place} onChange={(e) => setPlace(e.target.value)} required />
 
         <label>Область</label>
         <select value={regionId} onChange={(e) => setRegionId(e.target.value)} required>
@@ -171,15 +170,9 @@ const ApplicationForm = ({ onCancel }) => {
       </div>
 
       <div className="form-group">
-        {/* <label>Longitude:</label>
-        <input type="number" value={lon} onChange={(e) => setLon(e.target.value)}  />
-
-        <label>Latitude:</label>
-        <input type="number" value={lat} onChange={(e) => setLat(e.target.value)}  /> */}
-
         <label>Тип нарушения</label>
         <select value={typeViolationsId} onChange={(e) => setTypeViolationsId(e.target.value)} required>
-          <option value="">Выберите тип нарушения </option>
+          <option value="">Выберите тип нарушения</option>
           {violationsList.map((violation) => (
             <option key={violation.id} value={violation.id}>
               {violation.title}
@@ -189,9 +182,31 @@ const ApplicationForm = ({ onCancel }) => {
 
         <label>Приложить Фото/Видео доказательство</label>
         <input type="file" onChange={handleFileChange} />
-
-        <button type="submit">Submit</button>
       </div>
+
+      <div className="form-group">
+
+      <label>Адрес</label>
+        <input type="text" readonly='readonly' value={place} onChange={(e) => setPlace(e.target.value)} required />
+
+
+        <label>Долгота: {selectedCoordinate.lat}  </label>   
+        <label>Широта: {selectedCoordinate.lon}  </label>   
+        {isMapVisible ? (
+          <>
+            <MapComponent onCoordinateSelect={handleCoordinateSelect} setPlace={setPlace} />
+            <button type="button" onClick={handleSaveCoordinates}>
+              Сохранить
+            </button>
+          </>
+        ) : (
+          <button type="button" onClick={handleShowMap}>
+            Указать адрес
+          </button>
+        )}
+      </div>
+
+      <button type="submit">Submit</button>
     </form>
   );
 };
---
+++ src/Components/Applications/Component/MapComponent.js
@@ -0,0 +1,82 @@
+import React, { useState, useEffect, useRef } from 'react';
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+import Axios from 'axios';
+
+const MapComponent = ({ onCoordinateSelect, setPlace }) => {
+  const mapRef = useRef(null);
+  const markerRef = useRef(null);
+
+  useEffect(() => {
+    const leafletMap = L.map('map').setView([42.8731, 74.5945], 13);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(leafletMap);
+
+    leafletMap.on('click', handleMapClick);
+
+    mapRef.current = leafletMap;
+
+    return () => {
+      leafletMap.off();
+      leafletMap.remove();
+    };
+  }, []);
+
+  const handleMapClick = async (e) => {
+    const { lat, lng } = e.latlng;
+    let addressDetails;
+
+    try {
+      const response = await Axios.get(
+        `https://nominatim.openstreetmap.org/reverse?format=json&lat=${lat}&lon=${lng}&addressdetails=1&accept-language=ru`
+      );
+      addressDetails = response.data.address;
+
+      // Извлекаем необходимые компоненты адреса
+      const street = addressDetails.road || addressDetails.pedestrian || addressDetails.footway || '';
+      const houseNumber = addressDetails.house_number || '';
+      const crossroads = addressDetails.crossroads || '';
+
+      // Формируем окончательный адрес
+      const finalAddress = `${street} ${houseNumber}, ${crossroads}`;
+
+      setPlace(finalAddress.trim()); // Устанавливаем адрес в поле place
+    } catch (error) {
+      console.error('Error fetching address:', error.message);
+    }
+
+    if (markerRef.current) {
+      markerRef.current.setLatLng([lat, lng]);
+    } else {
+      const blueIcon = new L.Icon({
+        iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-blue.png',
+        shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+        iconSize: [25, 41],
+        iconAnchor: [12, 41],
+        popupAnchor: [1, -34],
+        shadowSize: [41, 41],
+      });
+
+      const newMarker = L.marker([lat, lng], { icon: blueIcon, draggable: true }).addTo(mapRef.current);
+      newMarker.bindPopup(addressDetails.display_name).openPopup();
+      newMarker.on('dragend', handleMarkerDragEnd);
+
+      markerRef.current = newMarker;
+    }
+
+    onCoordinateSelect({ lat, lon: lng });
+  };
+
+  const handleMarkerDragEnd = (e) => {
+    const { lat, lng } = e.target.getLatLng();
+    onCoordinateSelect({ lat, lon: lng });
+  };
+
+  return (
+    <div>
+      <div id="map" style={{ height: '500px', width: '100%' }}></div>
+      <p>Укажите адрес на карте</p>
+    </div>
+  );
+};
+
+export default MapComponent;
