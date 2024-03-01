45e9d6176b9d307b0126c1a54057458f4a76b01e
Amanturov7 <edi.amanturov@gmail.com>
2024-03-01 17:55:19 +0600

    avatar

--- src/Components/Account/UserProfile.js
+++ src/Components/Account/UserProfile.js
@@ -3,32 +3,90 @@ import axios from 'axios';
 
 function UserProfile() {
   const [userData, setUserData] = useState(null);
+  const [avatar, setAvatar] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [newAvatar, setNewAvatar] = useState(null);
+  const [uploadMessage, setUploadMessage] = useState('');
+  const [deleteMessage, setDeleteMessage] = useState('');
 
   useEffect(() => {
-    // Получение JWT токена из localStorage (предполагается, что он был сохранен там после успешной аутентификации)
     const token = sessionStorage.getItem('token');
-    console.log('JWT токен:', token); // Вывод токена на консоль
+
     if (token) {
-        // Устанавливаем JWT токен в заголовок Authorization
-        axios.defaults.headers.common['Authorization'] = `${token}`;
+      axios.defaults.headers.common['Authorization'] = `${token}`;
 
-        // Отправляем запрос к серверу для получения данных о пользователе
-        axios.get('http://localhost:8080/rest/user/user', {
-    params: {
-        token: token
+      axios.get('http://localhost:8080/rest/user/user', {
+        params: {
+          token: token
+        }
+      })
+      .then(response => {
+        setUserData(response.data);
+        setLoading(false);
+        fetchAvatar(response.data.id);
+      })
+      .catch(error => {
+        console.error('Ошибка при получении данных о пользователе:', error);
+        setLoading(false);
+      });
+    } else {
+      setLoading(false);
     }
-})
-.then(response => {
-    // Устанавливаем данные пользователя в состояние
-    setUserData(response.data);
-})
-.catch(error => {
-    console.error('Ошибка при получении данных о пользователе:', error);
-});
+  }, []);
 
+  const fetchAvatar = (userId) => {
+    axios.get(`http://localhost:8080/rest/attachments/download/avatar/user/${userId}`, {
+      responseType: 'arraybuffer'
+    })
+    .then(response => {
+      const imageUrl = URL.createObjectURL(new Blob([response.data]));
+      setAvatar(imageUrl);
+    })
+    .catch(error => {
+      console.error('Ошибка при загрузке аватара:', error);
+    });
+  };
+
+  const handleAvatarChange = (event) => {
+    setNewAvatar(event.target.files[0]);
+  };
+
+  const handleAvatarUpload = async () => {
+    if (!newAvatar) {
+      console.error('Файл не выбран');
+      return;
     }
-}, []);
 
+    try {
+      const formData = new FormData();
+      formData.append('file', newAvatar);
+
+      await axios.post(`http://localhost:8080/rest/user/${userData.id}/avatar`, formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+
+      setUploadMessage('Аватар успешно загружен');
+      fetchAvatar(userData.id); // Обновляем аватар после успешной загрузки
+    } catch (error) {
+      console.error('Ошибка при загрузке аватара:', error);
+    }
+  };
+
+  const handleAvatarDelete = async () => {
+    try {
+      await axios.delete(`http://localhost:8080/rest/user/${userData.id}/avatar`);
+      setDeleteMessage('Аватар успешно удален');
+      setAvatar(null); // Удаляем текущий аватар изображения после успешного удаления
+    } catch (error) {
+      console.error('Ошибка при удалении аватара:', error);
+    }
+  };
+
+  if (loading) {
+    return <p>Загрузка данных пользователя...</p>;
+  }
 
   return (
     <div>
@@ -38,10 +96,15 @@ function UserProfile() {
           <p>Имя: {userData.username}</p>
           <p>Роль: {userData.role}</p>
           <p>№: {userData.id}</p>
-
+          {avatar && <img src={avatar} alt="Аватар пользователя" />}
+          <input type="file" accept="image/*" onChange={handleAvatarChange} />
+          <button onClick={handleAvatarUpload}>Загрузить аватарку</button>
+          <button onClick={handleAvatarDelete}>Удалить аватарку</button>
+          {uploadMessage && <p>{uploadMessage}</p>}
+          {deleteMessage && <p>{deleteMessage}</p>}
         </div>
       ) : (
-        <p>Загрузка данных пользователя...</p>
+        <p>Данные пользователя не найдены.</p>
       )}
     </div>
   );
