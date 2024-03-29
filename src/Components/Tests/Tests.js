0b2d8d0c5df09bca29946b18b75dbae342b9876b
Amanturov7 <edi.amanturov@gmail.com>
2024-03-29 16:37:30 +0600

    tickets test

--- src/Components/Tests/Tests.js
+++ src/Components/Tests/Tests.js
@@ -1,15 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import Axios from 'axios';
-import TicketForm from './components/TicketForm';
-import TicketQuestions from './components/TestQuestion';
 
 const Tests = () => {
   const [ticketNumbers, setTicketNumbers] = useState([]);
-  const [selectedTicketNumber, setSelectedTicketNumber] = useState('');
-  const [questions, setQuestions] = useState([]);
   const [error, setError] = useState('');
-  const navigate = useNavigate();
 
   useEffect(() => {
     const fetchTicketNumbers = async () => {
@@ -23,46 +18,24 @@ const Tests = () => {
     fetchTicketNumbers();
   }, []);
 
-  useEffect(() => {
-    const fetchQuestionsByNumber = async () => {
-      if (!selectedTicketNumber) return; // Если номер билета не выбран, не делаем запрос
-      try {
-        const response = await Axios.get(`http://localhost:8080/rest/tickets/byNumber/${selectedTicketNumber}`);
-        setQuestions(response.data); // Сохраняем вопросы в состоянии компонента
-      } catch (error) {
-        setError('Ошибка при загрузке вопросов');
-      }
-    };
-    fetchQuestionsByNumber();
-  }, [selectedTicketNumber]);
 
-  const handleTicketNumberClick = (ticketNumber) => {
-    setSelectedTicketNumber(ticketNumber);
-    navigate(`/tickets/${ticketNumber}`);
-  };
 
   return (
     <div className='container'>
-      <TicketForm />
       <h2>Тесты ПДД</h2>
       <div>
         <h3>Выберите номер билета:</h3>
         {error && <p>{error}</p>}
         {ticketNumbers.map(ticketNumber => (
-          <div 
+          <Link 
             key={ticketNumber} 
-            onClick={() => handleTicketNumberClick(ticketNumber)}
+            to={`/tickets/${ticketNumber}`} // Перенаправляем на страницу с вопросами для выбранного билета
             style={{ cursor: 'pointer', marginRight: '10px' }}
           >
             Билет {ticketNumber}
-          </div>
+          </Link>
         ))}
       </div>
- 
-      {/* Проверяем, выбран ли билет, прежде чем отображать вопросы */}
-      {selectedTicketNumber && (
-        <TicketQuestions selectedTicketNumber={selectedTicketNumber} />
-      )}
     </div>
   );
 };
--- src/Components/Tests/components/TestQuestion.js
+++ src/Components/Tests/components/TestQuestion.js
@@ -1,65 +1,39 @@
 import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 import Axios from 'axios';
 
-const TicketQuestions = ({ selectedTicketNumber, handleAnswerSelect }) => {
+const TicketQuestions = () => {
+  const { ticketNumber } = useParams();
   const [questions, setQuestions] = useState([]);
   const [error, setError] = useState('');
-  const [questionImages, setQuestionImages] = useState({});
 
-
-
-  const loadQuestionImage = async (questionId) => {
-    try {
-      const response = await Axios.get(`http://localhost:8080/rest/attachments/download/tickets/${questionId}`, {
-        responseType: 'blob'
-      });
-      const imageUrl = URL.createObjectURL(response.data);
-      setQuestionImages(prevState => ({
-        ...prevState,
-        [questionId]: imageUrl
-      }));
-    } catch (error) {
-      console.error('Ошибка при загрузке изображения вопроса:', error);
-      // Если изображение не загружено, установите пустую строку в качестве URL
-      setQuestionImages(prevState => ({
-        ...prevState,
-        [questionId]: ''
-      }));
-    }
-  };
+  useEffect(() => {
+    const fetchQuestionsByNumber = async () => {
+      try {
+        const response = await Axios.get(`http://localhost:8080/rest/tickets/byNumber/${ticketNumber}`);
+        setQuestions(response.data);
+      } catch (error) {
+        setError('Ошибка при загрузке вопросов');
+      }
+    };
+    fetchQuestionsByNumber();
+  }, [ticketNumber]);
 
   return (
     <div className='container'>
-      <h2>Билет №{selectedTicketNumber}</h2>
+      <h2>Вопросы для билета №{ticketNumber}</h2>
       {error && <p>{error}</p>}
       <div>
         {questions.map(question => (
           <div key={question.id}>
             <h3>Вопрос {question.id}</h3>
-            {questionImages[question.id] && <img src={questionImages[question.id]} alt="Question" />}
             <p>{question.question}</p>
-            <div className="options-container">
-              <div className="option" onClick={() => handleAnswerSelect(question.option1)}>
-                <label className="option-label" htmlFor="option1">
-                  {question.option1}
-                </label>
-              </div>
-              <div className="option" onClick={() => handleAnswerSelect(question.option2)} >
-                <label className="option-label" htmlFor="option2">
-                  {question.option2}
-                </label>
-              </div>
-              <div className="option" onClick={() => handleAnswerSelect(question.option3)} >
-                <label className="option-label" htmlFor="option3">
-                  {question.option3}
-                </label>
-              </div>
-              <div className="option" onClick={() => handleAnswerSelect(question.option4)} >
-                <label className="option-label" htmlFor="option4">
-                  {question.option4}
-                </label>
-              </div>
-            </div>
+            <ul>
+              <li>{question.option1}</li>
+              <li>{question.option2}</li>
+              <li>{question.option3}</li>
+              <li>{question.option4}</li>
+            </ul>
           </div>
         ))}
       </div>
